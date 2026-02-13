0eefe1fd63bee09094501b6fbcaefd811b2b1100
Pepe Márquez Romero <pepe.marquezromero@protonmail.com>
2026-02-13 07:47:56 +0100

    remove obvious slop ai testing
    Signed-off-by: Pepe Márquez Romero <pepe.marquezromero@protonmail.com>

--- test/eink_test.exs
+++ test/eink_test.exs
@@ -2,17 +2,7 @@ defmodule EinkTest do
   use ExUnit.Case
   doctest EInk
 
-  test "EInk module exists" do
-    assert Code.ensure_loaded?(EInk)
-  end
-
-  test "EInk.Driver behaviour exists" do
-    assert Code.ensure_loaded?(EInk.Driver)
-  end
-
   test "UC8276 driver exists and has capabilities" do
-    assert Code.ensure_loaded?(EInk.Driver.UC8276)
-
     capabilities = EInk.Driver.UC8276.capabilities()
     assert capabilities.width == 400
     assert capabilities.height == 300
@@ -21,8 +11,6 @@ defmodule EinkTest do
   end
 
   test "UC8179 driver exists and has capabilities" do
-    assert Code.ensure_loaded?(EInk.Driver.UC8179)
-
     capabilities = EInk.Driver.UC8179.capabilities()
     assert capabilities.width == 800
     assert capabilities.height == 600
